d725e4aee56a372541cbff113ae5028e93b3e0fe
quady <decrefie@gmail.com>
2025-07-24 10:55:55 +0500

    Initial commit

--- todo/views.py
+++ todo/views.py
@@ -1,4 +1,16 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from todo import models
+from todo.models import TODOO
+
+
 
 def signup(request):
+    if request.method == 'POST':
+        fnm = request.POST.get('fnm')
+        emailid = request.POST.get('email')
+        pwd = request.POST.get('pwd')
+        print(fnm, emailid, pwd)
+
+
     return render(request, 'signup.html')
